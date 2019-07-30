81998cbced64c183438e7320ddcadada288f8969
Hugues de Valon <hugues.devalon@arm.com>
2019-07-30 17:43:25 +0100

    Service: Create service back components
    This commit:
    - creates backend handler component
    - creates dispatcher component
    
    Changes provided by:
    * Ionut Mihalcea (ionut.mihalcea@arm.com)
    * Hugues de Valon (hugues.devalon@arm.com)
    
    Change-Id: Ie09cdbe3ad483b4f5a7ba5cd9e23fc34a8491bd8
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- service/src/back/mod.rs
+++ service/src/back/mod.rs
@@ -12,5 +12,5 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-pub mod back;
-pub mod providers;
+pub mod backend_handler;
+pub mod dispatcher;
