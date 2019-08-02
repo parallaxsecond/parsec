fb7448d75b52dcd85ed449a08974545b107f9f13
Ionut Mihalcea <ionut.mihalcea@arm.com>
2019-08-02 13:04:05 +0100

    Service: Create service main functionality
    This commit implements a bare-bones service builder.
    
    Changes provided by:
    * Ionut Mihalcea (ionut.mihalcea@arm.com)
    * Hugues de Valon (hugues.devalon@arm.com)
    
    Change-Id: I8abf9c18eaccdeae6299c078540f47aa0150e2b0
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

---
+++ service/src/bin/main.rs
@@ -0,0 +1,63 @@
+// Copyright (c) 2019, Arm Limited, All Rights Reserved
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//          http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use interface::operations_protobuf::ProtobufConverter;
+use interface::requests::{BodyType, ProviderID};
+use service::back::{backend_handler::BackEndHandler, dispatcher::Dispatcher};
+use service::front::{
+    domain_socket::DomainSocketListener, front_end::FrontEndHandler, listener::Listen,
+};
+use service::providers::core_provider::CoreProvider;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+const VERSION_MINOR: u8 = 0;
+const VERSION_MAJOR: u8 = 1;
+
+/// Construct a hardcoded version of the service, containing only the core provider.
+fn construct_app() -> Box<dyn Listen> {
+    // Create the Core Provider and its associated BackEndHandler
+    let core_provider_id = ProviderID::CoreProvider;
+    let core_provider_backend = BackEndHandler {
+        provider: Box::from(CoreProvider {
+            version_min: VERSION_MINOR,
+            version_maj: VERSION_MAJOR,
+        }),
+        converter: Box::from(ProtobufConverter {}),
+        provider_id: core_provider_id,
+        content_type: BodyType::Protobuf,
+        accept_type: BodyType::Protobuf,
+        version_min: VERSION_MINOR,
+        version_maj: VERSION_MAJOR,
+    };
+
+    // Add the BackEndHandler structures to the Dispatcher
+    let mut backends: HashMap<ProviderID, BackEndHandler> = HashMap::new();
+    backends.insert(core_provider_id, core_provider_backend);
+
+    let dispatcher = Dispatcher { backends };
+    let front_end = FrontEndHandler { dispatcher };
+
+    Box::from(DomainSocketListener {
+        front_end_handler: Arc::from(front_end),
+        listener: None,
+    })
+}
+
+fn main() {
+    let mut listener = construct_app();
+
+    listener.init();
+    listener.run();
+}
