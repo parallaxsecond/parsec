475a19bd7ec856ae1325a088cf6705502e6e89c2
Hugues de Valon <hugues.devalon@arm.com>
2019-09-06 11:06:16 +0100

    Interface: Add key attributes
    This commit adds key attributes native structures and their conversion
    from and to protobuf format.
    
    Changes provided by:
    * Ionut Mihalcea (ionut.mihalcea@arm.com)
    * Hugues de Valon (hugues.devalon@arm.com)
    
    Change-Id: I833d55bee0ccc2172bd9e285f80df05c9d343883
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- interface/interface-rs/src/operations/mod.rs
+++ interface/interface-rs/src/operations/mod.rs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 mod ping;
+pub mod key_attributes;
 
 use crate::requests::{
     request::RequestBody,
--- interface/interface-rs/src/operations_protobuf/mod.rs
+++ interface/interface-rs/src/operations_protobuf/mod.rs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 mod convert_ping;
+mod convert_key_attributes;
 
 #[rustfmt::skip]
 mod generated_ops;
