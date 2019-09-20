cf226b84eeea5af3bf1ad42705cc1d865e7fb1e1
Ionut Mihalcea <ionut.mihalcea@arm.com>
2019-09-20 16:59:43 +0100

    Add ListProviders operation
    This commit adds the ListProviders functionality to the service.
    This includes the contracts and interface support and handling of
    the requests in the Core and Mbed providers. Providers are now
    required to provide a description for themselves.
    
    Changes provided by:
    * Ionut Mihalcea (ionut.mihalcea@arm.com)
    * Hugues de Valon (hugues.devalon@arm.com)
    
    Change-Id: I717d41cc4fa9a94965552a97eca4ae26b1c782fa
    Signed-off-by: Ionut Mihalcea <ionut.mihalcea@arm.com>

--- interface/interface-rs/src/operations/mod.rs
+++ interface/interface-rs/src/operations/mod.rs
@@ -20,6 +20,7 @@ mod export_public_key;
 mod destroy_key;
 mod asym_sign;
 mod asym_verify;
+mod list_providers;
 
 use crate::requests::{request::RequestBody, response::ResponseBody, Opcode, Result};
 pub use asym_sign::{OpAsymSign, ResultAsymSign};
@@ -28,11 +29,13 @@ pub use create_key::{OpCreateKey, ResultCreateKey};
 pub use destroy_key::{OpDestroyKey, ResultDestroyKey};
 pub use export_public_key::{OpExportPublicKey, ResultExportPublicKey};
 pub use import_key::{OpImportKey, ResultImportKey};
+pub use list_providers::{OpListProviders, ProviderInfo, ResultListProviders};
 pub use ping::{OpPing, ResultPing};
 
 /// Container type for operation conversion values, holding a native operation object
 /// to be passed in/out of a converter.
 pub enum NativeOperation {
+    ListProviders(OpListProviders),
     Ping(ping::OpPing),
     CreateKey(create_key::OpCreateKey),
     ImportKey(import_key::OpImportKey),
@@ -42,10 +45,26 @@ pub enum NativeOperation {
     AsymVerify(asym_verify::OpAsymVerify),
 }
 
+impl NativeOperation {
+    pub fn opcode(&self) -> Opcode {
+        match self {
+            NativeOperation::Ping(_) => Opcode::Ping,
+            NativeOperation::CreateKey(_) => Opcode::CreateKey,
+            NativeOperation::DestroyKey(_) => Opcode::DestroyKey,
+            NativeOperation::AsymSign(_) => Opcode::AsymSign,
+            NativeOperation::AsymVerify(_) => Opcode::AsymVerify,
+            NativeOperation::ImportKey(_) => Opcode::ImportKey,
+            NativeOperation::ExportPublicKey(_) => Opcode::ExportPublicKey,
+            NativeOperation::ListProviders(_) => Opcode::ListProviders,
+        }
+    }
+}
+
 /// Container type for result conversion values, holding a native result object to be
 /// passed in/out of the converter.
 #[derive(Debug)]
 pub enum NativeResult {
+    ListProviders(ResultListProviders),
     Ping(ping::ResultPing),
     CreateKey(create_key::ResultCreateKey),
     ImportKey(import_key::ResultImportKey),
@@ -53,6 +72,21 @@ pub enum NativeResult {
     DestroyKey(destroy_key::ResultDestroyKey),
     AsymSign(asym_sign::ResultAsymSign),
     AsymVerify(asym_verify::ResultAsymVerify),
+}
+
+impl NativeResult {
+    pub fn opcode(&self) -> Opcode {
+        match self {
+            NativeResult::Ping(_) => Opcode::Ping,
+            NativeResult::CreateKey(_) => Opcode::CreateKey,
+            NativeResult::DestroyKey(_) => Opcode::DestroyKey,
+            NativeResult::AsymSign(_) => Opcode::AsymSign,
+            NativeResult::AsymVerify(_) => Opcode::AsymVerify,
+            NativeResult::ImportKey(_) => Opcode::ImportKey,
+            NativeResult::ExportPublicKey(_) => Opcode::ExportPublicKey,
+            NativeResult::ListProviders(_) => Opcode::ListProviders,
+        }
+    }
 }
 
 /// Definition of the operations converters must implement to allow usage of a specific
--- service/src/providers/core_provider/mod.rs
+++ service/src/providers/core_provider/mod.rs
@@ -13,15 +13,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 use super::Provide;
+use interface::operations::ProviderInfo;
+use interface::operations::{OpListProviders, ResultListProviders};
 use interface::operations::{OpPing, ResultPing};
-use interface::requests::Result;
+use interface::requests::{ProviderID, Result};
 
 pub struct CoreProvider {
     version_min: u8,
     version_maj: u8,
+    providers: Vec<ProviderInfo>,
 }
 
 impl Provide for CoreProvider {
+    fn list_providers(&self, _op: OpListProviders) -> Result<ResultListProviders> {
+        Ok(ResultListProviders {
+            providers: self.providers.clone(),
+        })
+    }
+
+    fn describe(&self) -> ProviderInfo {
+        ProviderInfo {
+            id: ProviderID::CoreProvider,
+            description: String::from("Software provider that implements only administrative (i.e. no cryptographic) operations"),
+        }
+    }
+
     fn ping(&self, _op: OpPing) -> Result<ResultPing> {
         let result = ResultPing {
             supp_version_maj: self.version_maj,
@@ -36,6 +52,7 @@ impl Provide for CoreProvider {
 pub struct CoreProviderBuilder {
     version_maj: Option<u8>,
     version_min: Option<u8>,
+    providers: Option<Vec<ProviderInfo>>,
 }
 
 impl CoreProviderBuilder {
@@ -43,6 +60,7 @@ impl CoreProviderBuilder {
         CoreProviderBuilder {
             version_maj: None,
             version_min: None,
+            providers: None,
         }
     }
 
@@ -53,11 +71,32 @@ impl CoreProviderBuilder {
         self
     }
 
+    pub fn with_provider_info(mut self, provider_info: ProviderInfo) -> Self {
+        match self.providers {
+            Some(mut providers) => {
+                providers.push(provider_info);
+                self.providers = Some(providers);
+            }
+            None => {
+                let mut providers = Vec::new();
+                providers.push(provider_info);
+                self.providers = Some(providers);
+            }
+        }
+
+        self
+    }
+
     pub fn build(self) -> CoreProvider {
-        CoreProvider {
+        let mut core_provider = CoreProvider {
             version_maj: self.version_maj.expect("Version Maj missing"),
             version_min: self.version_min.expect("Version Min missing"),
-        }
+            providers: self.providers.expect("Providers info is missing"),
+        };
+
+        core_provider.providers.push(core_provider.describe());
+
+        core_provider
     }
 }
 
@@ -70,6 +109,7 @@ mod tests {
         let provider = CoreProvider {
             version_min: 8,
             version_maj: 10,
+            providers: Vec::new(),
         };
         let op = OpPing {};
         let result = provider.ping(op).unwrap();
