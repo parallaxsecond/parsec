19b22eeb64a2f44953f483aefd8d832268b69cd9
Robert Drazkowski <Robert.Drazkowski@globallogic.com>
2021-04-29 15:39:00 +0200

    Private key processing adjustment.
    Signed-off-by: Robert Drazkowski <Robert.Drazkowski@globallogic.com>

--- src/providers/cryptoauthlib/key_operations.rs
+++ src/providers/cryptoauthlib/key_operations.rs
@@ -4,7 +4,6 @@ use super::key_slot::KeySlotStatus;
 use super::Provider;
 use crate::authenticators::ApplicationName;
 use log::{error, warn};
-use openssl::ec::EcKey;
 use parsec_interface::operations::psa_key_attributes::{EccFamily, Type};
 use parsec_interface::operations::{psa_destroy_key, psa_generate_key, psa_import_key};
 use parsec_interface::requests::{ResponseStatus, Result};
@@ -163,17 +162,11 @@ fn extract_raw_key(key_type: Type, secret: &Secret<Vec<u8>>) -> Result<Secret<Ve
     let mut key = secret.expose_secret().to_vec();
 
     match key_type {
-        Type::Aes | Type::RawData => Ok(Secret::new(key)),
-        Type::EccKeyPair {
+        Type::Aes
+        | Type::RawData
+        | Type::EccKeyPair {
             curve_family: EccFamily::SecpR1,
-        } => {
-            match EcKey::private_key_from_der(&key) {
-                // Assumption - if key cannot be extracted, the argument (here: key) is invalid.
-                Err(_) => Err(ResponseStatus::PsaErrorInvalidArgument),
-                // The key was successfully  extracted from DER encoded string
-                Ok(ec_key) => Ok(Secret::new(ec_key.private_key().to_vec())),
-            }
-        }
+        } => Ok(Secret::new(key)),
         Type::EccPublicKey {
             curve_family: EccFamily::SecpR1,
         } => match key.len() {
@@ -186,40 +179,6 @@ fn extract_raw_key(key_type: Type, secret: &Secret<Vec<u8>>) -> Result<Secret<Ve
         },
         _ => Err(ResponseStatus::PsaErrorNotSupported),
     }
-}
-
-#[test]
-fn test_extract_raw_ecc_private_key() {
-    let ecc_key_der_array: Secret<Vec<u8>> = Secret::new(
-        [
-            0x30, 0x77, 0x02, 0x01, 0x01, 0x04, 0x20, 0x26, 0xc8, 0x82, 0x9e, 0x22, 0xe3, 0x0c,
-            0xa6, 0x3d, 0x29, 0xf5, 0xf7, 0x27, 0x39, 0x58, 0x47, 0x41, 0x81, 0xf6, 0x57, 0x4f,
-            0xdb, 0xcb, 0x4d, 0xbb, 0xdd, 0x52, 0xff, 0x3a, 0xc0, 0xf6, 0x0d, 0xa0, 0x0a, 0x06,
-            0x08, 0x2a, 0x86, 0x48, 0xce, 0x3d, 0x03, 0x01, 0x07, 0xa1, 0x44, 0x03, 0x42, 0x00,
-            0x04, 0x01, 0xf7, 0x69, 0xe2, 0x40, 0x3a, 0xeb, 0x0d, 0x64, 0x3e, 0x81, 0xb8, 0xda,
-            0x95, 0xb0, 0x1c, 0x25, 0x80, 0xfe, 0xa3, 0xd3, 0xd0, 0x5b, 0x2f, 0xef, 0x6a, 0x31,
-            0x9c, 0xa9, 0xca, 0x5d, 0xe5, 0x2b, 0x4b, 0x49, 0x2c, 0x24, 0x2c, 0xef, 0xf4, 0xf2,
-            0x3c, 0xef, 0xfa, 0x08, 0xa7, 0xb4, 0xc6, 0xe0, 0xce, 0x73, 0xac, 0xd0, 0x69, 0xd4,
-            0xcc, 0xa8, 0xd0, 0x55, 0xee, 0x6c, 0x65, 0xb5, 0x71,
-        ]
-        .to_vec(),
-    );
-    let ecc_priv_key: [u8; 32] = [
-        0x26, 0xc8, 0x82, 0x9e, 0x22, 0xe3, 0x0c, 0xa6, 0x3d, 0x29, 0xf5, 0xf7, 0x27, 0x39, 0x58,
-        0x47, 0x41, 0x81, 0xf6, 0x57, 0x4f, 0xdb, 0xcb, 0x4d, 0xbb, 0xdd, 0x52, 0xff, 0x3a, 0xc0,
-        0xf6, 0x0d,
-    ];
-    let ecc_priv_key_ext = extract_raw_key(
-        Type::EccKeyPair {
-            curve_family: EccFamily::SecpR1,
-        },
-        &ecc_key_der_array,
-    )
-    .unwrap();
-    assert_eq!(
-        ecc_priv_key.to_vec(),
-        ecc_priv_key_ext.expose_secret().to_owned()
-    );
 }
 
 #[test]
