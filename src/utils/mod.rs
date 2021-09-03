750ffb1397c219fbe47fdc9c0c6f17180fd6e0c5
Matt Davis <matt.davis@arm.com>
2021-09-03 12:11:34 +0100

    Added provider name config tests.
    Added config tests that cover different values given to provider names within the config.
    These tests cover two providers where:
    - Same provider type, same name. (Throw error)
    - Same provider type, different name. (Allow)
    - Same provider type, same default name. (Throw error).
    - Different types, same name. (Throw error). (We could allow this in the future if we wish).
    - Different types, different default names. (Allow)
    
    Closes #496
    
    Signed-off-by: Matt Davis <matt.davis@arm.com>

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -5,6 +5,16 @@ pub mod cli;
 pub mod config;
 mod global_config;
 mod service_builder;
+#[cfg(all(
+    feature = "mbed-crypto-provider",
+    feature = "pkcs11-provider",
+    feature = "tpm-provider",
+    feature = "cryptoauthlib-provider",
+    feature = "trusted-service-provider",
+    feature = "direct-authenticator"
+))]
+#[cfg(test)]
+mod tests;
 
 pub use global_config::GlobalConfig;
 pub use service_builder::ServiceBuilder;
