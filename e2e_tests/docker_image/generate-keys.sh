9c44b24c8994228fb80ded4ad189f0083929b9fd
Mohamed Omar Asaker <mohamed.omarasaker@arm.com>
2022-07-19 13:24:14 +0100

    Build Parsec service with trusted service in generate_keys script
    - Move keys and mapping files into a separate folders to avoid conflict
      between TS mock and other providers
    - Add comments
    - Re-organize script
    
    Signed-off-by: Mohamed Omar Asaker <mohamed.omarasaker@arm.com>

--- e2e_tests/docker_image/generate-keys.sh
+++ e2e_tests/docker_image/generate-keys.sh
@@ -14,9 +14,16 @@ mkdir /tmp/create_keys
 # with today's version.
 git clone https://github.com/parallaxsecond/parsec.git --branch 0.7.0 /tmp/create_keys/parsec
 cd /tmp/create_keys/parsec
+git submodule update --init --recursive
+
+# We use the Parsec Tool to create one RSA and one ECC key per provider,
+# when it is possible.
+cargo install parsec-tool
+
+# Build service with all providers (trusted-service-provider isn't included)
 cargo build --features "all-providers, all-authenticators"
 
-# Start the service with all providers
+# Start the service with all providers (trusted-service-provider isn't included)
 tpm_server &
 sleep 5
 tpm2_startup -c -T mssim
@@ -28,10 +35,9 @@ SLOT_NUMBER=`softhsm2-util --show-slots | head -n2 | tail -n1 | cut -d " " -f 2`
 find . -name "*toml" -not -name "Cargo.toml" -exec sed -i "s/^# slot_number.*$/slot_number = $SLOT_NUMBER/" {} \;
 cd ../
 ./target/debug/parsec -c e2e_tests/provider_cfg/all/config.toml &
+sleep 2
 
-# We use the Parsec Tool to create one RSA and one ECC key per provider,
-# when it is possible.
-cargo install parsec-tool
+# Generate keys for all providers (trusted-service-provider isn't included)
 parsec-tool -p 1 create-rsa-key -k rsa
 parsec-tool -p 1 create-ecc-key -k ecc
 parsec-tool -p 2 create-rsa-key -k rsa
@@ -48,12 +54,18 @@ tpm2_shutdown -T mssim
 sleep 2
 pkill tpm_server
 
-# Cleanup to reduce image's size
-cargo uninstall parsec-tool
-cp -r /tmp/create_keys/parsec/mappings /tmp
 # Mbed Crypto creates keys in the current directory.
-cp -r /tmp/create_keys/parsec/0000000000000002.psa_its /tmp
-cp -r /tmp/create_keys/parsec/0000000000000003.psa_its /tmp
+mv /tmp/create_keys/parsec/mappings /tmp
+mv /tmp/create_keys/parsec/0000000000000002.psa_its /tmp
+mv /tmp/create_keys/parsec/0000000000000003.psa_its /tmp
 # The TPM server state needs to be passed to the tested service
-cp -r /tmp/create_keys/parsec/NVChip /tmp
+mv /tmp/create_keys/parsec/NVChip /tmp
+
+# Build the service with trusted service provider
+cargo build --features "trusted-service-provider, all-authenticators"
+# Start the service with trusted service provider
+./target/debug/parsec -c e2e_tests/provider_cfg/trusted-service/config.toml &
+
+# Cleanup to reduce image's size
 rm -rf /tmp/create_keys
+cargo uninstall parsec-tool
