969fd95f24ddbba71bf14a501406f2f47dc7dffc
Mohamed Omar Asaker <mohamed.omarasaker@arm.com>
2022-08-02 11:13:53 +0100

    Don't run key_mapping tests for CAL provider
    Signed-off-by: Mohamed Omar Asaker <mohamed.omarasaker@arm.com>

--- ci.sh
+++ ci.sh
@@ -104,8 +104,12 @@ run_old_e2e_tests() {
 }
 
 run_key_mappings_tests() {
-    echo "Execute key mappings tests"
-    RUST_BACKTRACE=1 cargo test $TEST_FEATURES --manifest-path ./e2e_tests/Cargo.toml key_mappings
+    # There is no keys generated for CryptoAuthLib yet.
+    # This condition should be removed when the keys are generated for the CAL provider
+    if ! [[ "$PROVIDER_NAME" = "cryptoauthlib" ]]; then
+        echo "Execute key mappings tests"
+        RUST_BACKTRACE=1 cargo test $TEST_FEATURES --manifest-path ./e2e_tests/Cargo.toml key_mappings
+    fi
 }
 
 setup_mappings() {
